bb8ed38c9a98f1c2a1b681ed4454d13e7383af53
Abhinav Tripathi <genuineabhinav@gmail.com>
2015-10-13 00:25:38 +0530

    Made a one sided app for timely location saving.

--- app/src/main/java/iit/iotgroup/cloudtry/aws/aws_cloud_1/StoreToDB.java
+++ app/src/main/java/iit/iotgroup/cloudtry/aws/aws_cloud_1/StoreToDB.java
@@ -3,9 +3,7 @@ package iit.iotgroup.cloudtry.aws.aws_cloud_1;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.content.Context;
 import android.os.AsyncTask;
-import android.widget.Toast;
 
 import com.amazonaws.services.simpledb.model.CreateDomainRequest;
 import com.amazonaws.services.simpledb.model.PutAttributesRequest;
@@ -13,10 +11,10 @@ import com.amazonaws.services.simpledb.model.ReplaceableAttribute;
 
 public class StoreToDB extends AsyncTask<LocationData, Void, Void>  {
 
-	public static Context appctx;
+	//public static Context appctx;
 	
 	// 2. Create Domain and save movie information in domain
-	public static void saveMovie(double latitude, double longitude, String timestamp)
+	public static void saveLocation(double latitude, double longitude, String timestamp)
 	{
 		try {
 			 Connection.getAwsSimpleDB().createDomain(new CreateDomainRequest( Utilities.GPS_LOCATION_DOMAIN_NAME));
@@ -33,14 +31,14 @@ public class StoreToDB extends AsyncTask<LocationData, Void, Void>  {
 	
 	@Override
 	protected Void doInBackground(LocationData... params) {
-		saveMovie(params[0].latitude, params[0].longitude, params[0].timeStamp);
+		saveLocation(params[0].latitude, params[0].longitude, params[0].timeStamp);
 		return null;
 	}
 
 	@Override
 	protected void onPostExecute(Void param)
 	{
-		Toast.makeText(appctx, "Location stored successfully!", Toast.LENGTH_LONG).show();
+		//Toast.makeText(appctx, "Location stored successfully!", Toast.LENGTH_LONG).show();
 	}
 	
 }
--- app/src/main/java/iit/iotgroup/cloudtry/aws/aws_cloud_1/Utilities.java
+++ app/src/main/java/iit/iotgroup/cloudtry/aws/aws_cloud_1/Utilities.java
@@ -1,8 +1,13 @@
 package iit.iotgroup.cloudtry.aws.aws_cloud_1;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Main Activity which opens when app opens
@@ -18,15 +23,47 @@ public class Utilities {
     public static String ACCESS_KEY = "AKIAJ4WB2PVVVTKSJLFQ";
     public static String SECRET_KEY = "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";
 
-    public static String getUserName()
-    {
-        return "Abhinav";
-    }
-
     public static boolean isNetworkAvailable(Context ctx)
     {
         ConnectivityManager ctvMngr = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo aNetInfo = ctvMngr.getActiveNetworkInfo();
         return aNetInfo != null && aNetInfo.isAvailable();
+    }
+
+    public static final String STORAGE_INTERVAL_PREF_KEY = "NUM_ALARMS_PREFERENCE";
+    public static int getPrefInt(Context ctx, String prefKey)
+    {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ctx.getApplicationContext());
+        return sp.getInt(prefKey, 0);
+    }
+    public static void savePrefInt(Context ctx, String prefKey, int prefValue)
+    {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ctx.getApplicationContext());
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt(prefKey, prefValue);
+        editor.apply();
+    }
+
+    public static void storeUpdateInterval(int numAlarm, Context ctx)
+    {
+        savePrefInt(ctx, STORAGE_INTERVAL_PREF_KEY, numAlarm);
+    }
+    public static int getUpdateInterval(Context ctx)
+    {
+        return getPrefInt(ctx, STORAGE_INTERVAL_PREF_KEY);
+    }
+    public static long getNextMinForAlarm(Context ctx)
+    {
+        int interval = getUpdateInterval(ctx);
+        if(interval<=0)
+            return 0;
+        // time at which alarm will be scheduled here alarm is scheduled at 1 day from current time,
+        // we fetch  the current time in milliseconds and added 1 day time
+        // i.e. 24*60*60*1000= 86,400,000   milliseconds in a day
+        Calendar cal = new GregorianCalendar();
+        int h = cal.get(Calendar.HOUR_OF_DAY), m = cal.get(Calendar.MINUTE);
+        int totT = h*60 + m;
+        int n = totT/interval;
+        return (n+1)*interval - totT;
     }
 }
